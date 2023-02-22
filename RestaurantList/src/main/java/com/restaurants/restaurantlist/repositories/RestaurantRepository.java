1e273f69836ed36ead556130f16e0861947b82c9
bahjason <96155150+bahjason@users.noreply.github.com>
2023-02-22 14:12:21 -0500

    Delete RestaurantList directory

--- RestaurantList/src/main/java/com/restaurants/restaurantlist/repositories/RestaurantRepository.java
+++
@@ -1,7 +0,0 @@
-package com.restaurants.restaurantlist.repositories;
-
-import com.restaurants.restaurantlist.data.Restaurant;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RestaurantRepository extends CrudRepository<Restaurant, Integer> {
-}
